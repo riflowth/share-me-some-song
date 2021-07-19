f79b6ced5d3d7209df502ddc9f1b0dbca70f0ba7
riflowth <riflowth@gmail.com>
2021-07-20 03:31:25 +0700

    feat: add optical illusion

--- src/components/common/WebContent.js
+++ src/components/common/WebContent.js
@@ -1,6 +1,6 @@
 export default function WebContent({ children }) {
   return (
-    <main className="container m-auto transform -translate-y-16 sm:translate-y-0">
+    <main className="container m-auto transform -translate-y-16 sm:-translate-y-12">
       {children}
     </main>
   )
